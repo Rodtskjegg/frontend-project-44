2cb3fc853ba8f689bd74c378fdbd94cc4727426c
rodtskjegg <sergei.polikin@ringcentral.com>
2024-08-23 19:35:23 +0400

    add folder games

---
+++ src/games/progression.js
@@ -0,0 +1,26 @@
+export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const generateProgression = (start, step, length) => {
+  const progression = [];
+  for (let i = 0; i < length; i++) {
+    progression.push(start + i * step);
+  }
+  return progression;
+};
+
+const generateRound = () => {
+  const progressionLength = getRandomNumber(5, 10);  // Длина прогрессии от 5 до 10 чисел
+  const start = getRandomNumber(1, 10);  // Начальное число
+  const step = getRandomNumber(1, 10);   // Шаг прогрессии
+  const progression = generateProgression(start, step, progressionLength);
+
+  const hiddenIndex = getRandomNumber(0, progressionLength - 1);
+  const correctAnswer = progression[hiddenIndex];
+  progression[hiddenIndex] = '..';
+
+  const question = progression.join(' ');
+
+  return [question, String(correctAnswer)];
+};
+
+export default generateRound;
