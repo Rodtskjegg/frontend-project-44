c8b2498379ffb7ed70cec15454870060304280bf
rodtskjegg <sergei.polikin@ringcentral.com>
2024-09-26 21:02:42 +0400

    rename bc and be to calc and even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 import playGame from '../src/engine.js';
-import { description, generateRound } from '../src/games/be.js';
+import { description, generateRound } from '../src/games/even.js';
 
 playGame(description, generateRound);
